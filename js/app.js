7cb3e6539b5c799b0285cc4ec1666b8033eb768c
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-10 13:08:47 -0600

    Finished #4 - Pythagoras

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@
 
 // Palindrome
 function checkPalindrome (palindrome) {
+	// convert palindrom to lowercase and turn to an array of characters
 	smallCasePal = palindrome.toLowerCase().split("").join("")
+	// create another array that is the reverse of the first
 	var reversePalindrome = palindrome.split("").reverse().join("").toLowerCase()
+	// compare the arrays, if they match, return true, if not return false
 	if (smallCasePal === reversePalindrome) {
 		return true;
 	}; 
@@ -17,10 +20,23 @@ function checkPalindrome (palindrome) {
 
 // Digit Sum
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// reducer is taking 2 numbers in the array, adding them to each other and then adding to current value, and so on. NEED THIS FUNCTION TO USE .reduce().
 
 function sumDigits(number) {
+	// turn the number into an array of WHOLE integers
+	// How: make a string, split the string, use . map to make numbers (from library)
 	let numArray = number.toString(10).split("").map(Number)
+	// 10 = radix number (so 0-9)
 	return numArray.reduce(reducer);
+}
+
+// Pythagoras
+function calculateSide(sideA, sideB) {
+	// sideA squared + sideB squared = sideC squared
+	let sideCSquare = (Math.pow(sideA, 2) + Math.pow(sideB, 2))
+	// return square root of squareC
+	let sideC = Math.sqrt(sideCSquare)
+	return sideC
 }
 
 
