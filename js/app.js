fa6480bed26e5f5a332d63d34a29569989b8fb0d
ethan375 <ethanebel1@gmail.com>
2017-12-10 11:51:14 -0600

    Finished #1: verbal questions

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//verbal question
+
+//a parameter is anything in the parenthesis in the function decleration. An argument is what is passed into the function when called.
+
+//console.log prints out the function & return returns the value of the function
+
+//scope determines what variable are accessible. ie a global variable would be accessible to all function, while a local variable is only available to one function
+
+
