8985141c08e9676305c8de31fd97b0f2b59be397
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-09 22:46:28 -0600

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Verbal Questions
+// a. What is the difference between a parameter and an argument? A parameter is is the argument that a function will take when called.
+
+// b. Within a function, what is the difference between return and console.log? Return is the actual output of a block of code, console.log is something that is just being printed to the console.
+
+// c. Explain the concept of scope in four (or fewer) concise sentences. Scope comes in two varieties: local and global. If we think about it in terms of a box containing scope, global scope is available both inside and out side of the box. Local scope is only available inside of the box.
+
