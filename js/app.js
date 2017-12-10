9fd83fc4b862288f164e8c29270fde64e585ccfd
sergio <scrummish@gmail.com>
2017-12-10 16:06:26 -0600

    Finished #5: Sum Array

--- js/app.js
+++ js/app.js
@@ -4,34 +4,36 @@
 // c. The scope of a variable is local to its code block {}. Scope is meant to keep variables from getting unwanted changes by other lines of code.
 
 // 2. Palindrome --------------------------------------------
-// function palindrome(str) {
-// 	str = str.toLowerCase();
-// 	let arr = str.split("");
-// 	arr = arr.reverse();
-// 	let str2 = arr.join("");
+function palindrome(str) {
+	str = str.toLowerCase();
+	let arr = str.split("");
+	arr = arr.reverse();
+	let str2 = arr.join("");
 
-// 	if (str2 == str) {
-// 	return true;
-// 	}	return false;
-// }
+	if (str2 == str) {
+	return true;
+	}	return false;
+}
 
-// console.log("2. Palindrome:");
-// console.log("Mom: " + palindrome("moM"));
-// console.log("Aunt: " + palindrome("AuNt"));
+console.log("2. Palindrome:");
+console.log("Mom: " + palindrome("moM"));
+console.log("Aunt: " + palindrome("AuNt"));
+console.log(" ");
 
 
 // 3. Digit Sum -----------------------------------------------
-// function sumDigits(num) {
-// 	let arr = num.toString().split("");
-// 	let sum = 0;
+function sumDigits(num) {
+	let arr = num.toString().split("");
+	let sum = 0;
 	
-// 	for(var i = 0; i<arr.length; i++) {
-//     sum += parseInt(arr[i]);
-// 	} return sum
-// }
+	for(var i = 0; i<arr.length; i++) {
+    sum += parseInt(arr[i]);
+	} return sum
+}
 
-// console.log("3. Digit Sum:");
-// console.log("555 equals " + sumDigits(555));
+console.log("3. Digit Sum:");
+console.log("555 equals " + sumDigits(555));
+console.log(" ");
 
 // 4. Pythargoras ---------------------------------------------
 function calculateSide(sideA, sideB){
@@ -40,6 +42,22 @@ function calculateSide(sideA, sideB){
 
 console.log("4. Pythagoras:");
 console.log("8 and 6 = " + calculateSide(8,6));
+console.log(" ");
+
+// 5. Sum Array -----------------------------------------------
+function sumArray(arr){
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	};
+	return sum;
+}
+
+console.log("5. Sum Array:");
+console.log("Sum of the digits 1,2,3,4,5,6 equals " + sumArray([1, 2, 3, 4, 5, 6]));
+console.log(" ");
+
 
 
 
