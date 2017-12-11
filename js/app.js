08955691920d0be2a75cd7d950ecb8ed67c2e431
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-10 22:41:09 -0600

    Finished #1: Verbal questions

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+// 1. Verbal Questions 
+
+// a parameter is a variable that is part of the functions signature.
+// an argument is the variable that is used in calling the function to output in response to the given arguments
+
+// console.log simply prints out a result to the console
+// return returns a result to be used or manipulated more in the code
+
+// scope regards how and where you can use certain functions and variables.
+// When a function or variable is stated while nested in a larger function than it cannot be used outside of where it is stated (local). 
+// When a function or variable is stated un nested it can be used anywhere (global).
+
