e01d553b84ef9abb941dcfa397fd80381591ba8b
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-10 23:34:20 -0600

    Finished #2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@
 // c. Concept of Scope ~ scope is the context in which the values and expressions are visable (or useable) for reference.
 // The clearest example of this is local variables that are only defined inside a given function or a global variable
 // which can be accesed from anywhere in the code.
+
+//*****************PALINDROME*********************************
+function checkPalindrome (str){
+    var caseString = str.toLowerCase().split("").filter(c => c != ' ').join("");
+    var reverseString = caseString.split("").reverse().join("");
+    return reverseString == caseString;
+}
+console.log( checkPalindrome("RaDar") );
+// convert all to lowercase then turn to array with split then use filter to loop through each charater and removes the space charaters and then use join to convert back to a string // turn back into array to reverse then back into a string // then check for equality
