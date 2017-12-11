167bf424b5b89e5184de409a67e1f290e9bceee4
Anthony <anthonyjlower@gmail.com>
2017-12-10 18:12:29 -0600

    Finished #1: Verbal questions

---
+++ js/app.js
@@ -0,0 +1,6 @@
+	// 1. verbal questions
+
+// a. parameters are the aliases for the values that will be passed through a funciont, arugments are the actual values
+// b. return will stop a function from executing where console.log only prints the called parameter
+// c. Scope affects where a variable can be used. Variables used outside of a funciton have global scope and can be used anwyere. Functions called inside of functions only have scope within that funciton.
+
