bba1bc32431f6d517ed3121131d63c8908c220d3
Charles <because@charlesis.me>
2017-12-11 16:51:54 -0600

    Finished #5 - Sum Array

--- js/app.js
+++ js/app.js
@@ -13,10 +13,30 @@ Console.log prints out the result of a line of code and continues (debugging), R
 
 Scope has two loactions a "global" and a "Local", global variables are inclusive so any fucntion can use them and local variables are exclusive so no other fuction can use them.
 
-*/
+
 
 function checkPalindrome (str){
      var caseString = str.toLowerCase().split("").filter(c => c != ' ').join("");
      var reverseString = caseString.split("").reverse().join("");
      return reverseString == caseString;
  }
+
+
+<!-- Digital Sum -->
+
+*/
+
+function dS(num) {
+
+    var tS = num.toString();
+    var splitNum = tS.split("");
+
+
+    let sum = 0;
+    for (i = 0; i < splitNum.length; i++) {
+        sum = sum + parseInt(splitNum[i]);
+
+    }
+    return sum;
+}
+
