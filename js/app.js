fd5f0c5231c56ca13812d6a0c79fc05769322cf7
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-10 21:09:10 -0600

    Finished #1: Verbal questions

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//******************VERBAL QUESTIONS*************************
+// a. Paramter and Argument difference ~ a parameter is a variable you declare in the parentheses of a function and
+// the argument is the value you assign to the parameter.
+
+// b. Within a function the difference between RETURN and CONSOLE.LOG ~ console.log will log anything into the console.
+// Inside of a function if will print any value you place inside it. The return statement will end the function and
+// retun a specified value to the function caller.
+
+// c. Concept of Scope ~ scope is the context in which the values and expressions are visable (or useable) for reference.
+// The clearest example of this is local variables that are only defined inside a given function or a global variable
+// which can be accesed from anywhere in the code.
